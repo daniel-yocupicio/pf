d0a440b459aba21b17ad312ad2f37fe8942b54ae
unknown <dag_98@hotmail.com>
2022-07-07 20:40:27 -0500

    add project

---
+++ petmyheart/App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import RegisterScreen from './src/screens/registerScreen';
+
+const App = () => {
+  return <RegisterScreen />;
+};
+
+export default App;
